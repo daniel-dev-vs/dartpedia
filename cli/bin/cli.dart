c0899c2bec43da0b953d2c6200fb16029d3679d0
Daniel <danenoisturbera@gmail.com>
2026-06-17 21:53:52 -0300

    first commit

---
+++ cli/bin/cli.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+
+const version = '0.0.1';
+
+void main(List<String> arguments) async {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+
+  commandRunner.run(arguments);
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'wikipedia <ARTICLE-TITLE>'",
+  );
+}
+
+void searchWikipedia(List<String>? arguments) async {
+  final String? articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    final inputFromStdin = stdin.readLineSync();
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provide. Exiting...');
+      return;
+    }
+    articleTitle = inputFromStdin;
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  var articleContent = await getWikipediaArticle(articleTitle);
+
+  print(articleContent);
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org',
+    '/api/rest_v1/page/summary/$articleTitle',
+  );
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  return 'Error: failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
+}
+
+bool isValidArguments(List<String>? arguments) {
+  if (arguments == null || arguments.isEmpty) {
+    return false;
+  }
+  return true;
+}
---
+++ command_runner/lib/src/command_runner_base.dart
@@ -0,0 +1,35 @@
+import 'arguments.dart';
+import 'dart:collection';
+import 'dart:io';
+
+class CommandRunner {
+  final Map<String, Command> _commands = <String, Command>{};
+  
+  UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+  String get usage {
+    final exeFile = Platform.script.path.split('/').last;
+    return 'Usage: dart bin/$exeFile <command> [commandArgs?] [...options?]';
+  }
+
+  Future<void> run(List<String> input) async {
+    final ArgResults results = parse(input);
+
+    if (results.command != null) {
+      Object? output = await results.command!.run(results);
+      print(output.toString());
+    }
+  }
+
+  void addCommand(Command command) {
+    _commands[command.name] = command;
+    command.runner = this;
+  }
+
+  ArgResults parse(List<String> input) {
+    var results = ArgResults();
+    results.command = _commands[input.first];
+    return results;
+  }
+}
